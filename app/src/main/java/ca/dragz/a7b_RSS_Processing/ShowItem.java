4fd007cc1508ab0819408df3ed3b72dba140434e
Dustin Martens <dmartens.it@gmail.com>
2020-02-29 19:10:20 -0600

    Text color gets update on ShowItem activity now

--- app/src/main/java/ca/dragz/a7b_RSS_Processing/ShowItem.java
+++ app/src/main/java/ca/dragz/a7b_RSS_Processing/ShowItem.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,11 +15,15 @@ public class ShowItem extends AppCompatActivity {
 
     Toolbar toolbar;
 
+    private SharedPreferences sharedPreferences;
+
     private TextView tvTitle;
     private TextView tvDescription;
     private TextView tvPubDate;
     private TextView tvLink;
     private TextView tvPrice;
+
+    private int color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +33,22 @@ public class ShowItem extends AppCompatActivity {
         //make sure to use a theme with no action bar (set in your manifest)
         toolbar = findViewById(R.id.myToolbar2);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("A7b - RSS Processing");
+
+        sharedPreferences = getSharedPreferences("RSS Processing", MODE_PRIVATE);
+
+        color = getTextColor();
 
         Intent intent = getIntent();
 
         tvTitle = findViewById(R.id.tvTitle);
+        tvTitle.setTextColor(color);
         tvDescription = findViewById(R.id.tvDescription);
+        tvDescription.setTextColor(color);
         tvPubDate = findViewById(R.id.tvPubDate);
+        tvPubDate.setTextColor(color);
         tvLink = findViewById(R.id.tvLink);
         tvPrice = findViewById(R.id.tvPrice);
+        tvPrice.setTextColor(color);
 
         tvTitle.setText(intent.getStringExtra("title"));
         tvDescription.setText(intent.getStringExtra("description"));
@@ -56,5 +69,13 @@ public class ShowItem extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.appbar, menu);
         return true;
+    }
+
+    private int getTextColor() {
+        int red = sharedPreferences.getInt("color_red", 0);
+        int green = sharedPreferences.getInt("color_green", 0);
+        int blue = sharedPreferences.getInt("color_blue", 0);
+
+        return Color.rgb(red, green, blue);
     }
 }
